a5da6a34ba77d612d7c487c74b3a9bd6400e181f
calebe <calebe.fazzani.facul@gmail.com>
2020-08-29 03:15:34 -0300

    Atualização para usar valor do JSON requirido no processo

--- src/main/java/com/example/primos/dominio/CalculaPrimosService.java
+++ src/main/java/com/example/primos/dominio/CalculaPrimosService.java
@@ -1,35 +1,52 @@
 package com.example.primos.dominio;
 
 import java.io.IOException;
-import java.util.stream.IntStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import com.example.primos.entidades.NumeroPrimo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 @Service
 public class CalculaPrimosService {
+	private static final String BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
 	private static final Logger LOGGER = LogManager.getLogger(CalculaPrimosService.class);
-    public boolean isPrimo(Integer numero) {
-    	getUrl("https://jsonplaceholder.typicode.com/todos/1");
-        if(numero == null) return false;
-        return IntStream.rangeClosed(1, numero).filter(el -> numero%el == 0).count() == 2;
-    }
-    
-    private void getUrl(String url) {
-    	OkHttpClient client = new OkHttpClient();
-    	Request request = new Request.Builder()
-    						  .url(url)
-    						  .build();
-    	try(Response response = client.newCall(request).execute()) {
-    		//System.out.println(response.body().string());
-    		LOGGER.info(response.body().string());
-    	} catch(IOException e) {
-    		e.printStackTrace();
-    	}
-    }
+	private static final String FIELD = "id";
+
+	@SuppressWarnings("unchecked")
+	public NumeroPrimo getPrimo() throws Exception {
+		NumeroPrimo numeroPrimo = new NumeroPrimo();
+		String response = getRequestBody();
+		Map<String, Object> body = new ObjectMapper().readValue(response, HashMap.class);
+		numeroPrimo.setValor((Integer) body.get(FIELD));
+		return numeroPrimo;
+	}
+
+	private String getRequestBody() {
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder().url(getUrl()).build();
+		try (Response response = client.newCall(request).execute()) {
+			String stringBody = response.body().string();
+			LOGGER.info(stringBody);
+			return stringBody;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private String getUrl() {
+		Integer id = new Random().nextInt(10) + 1;
+		return BASE_URL + id;
+
+	}
 }
--- src/main/java/com/example/primos/requests/CalculoRequest.java
+++ src/main/java/com/example/primos/requests/CalculoRequest.java
@@ -1,13 +1,12 @@
 package com.example.primos.requests;
 
-import javax.websocket.server.PathParam;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,23 +16,23 @@ import com.example.primos.entidades.NumeroPrimo;
 @RestController
 @RequestMapping("/api")
 public class CalculoRequest {
-	
+	private static final Logger LOGGER = LogManager.getLogger(CalculoRequest.class);
 	@Autowired
 	private CalculaPrimosService service;
 	
 	@GetMapping("/")
-	public ResponseEntity<NumeroPrimo> getCalculo(@PathParam(value = "valor") NumeroPrimo valor) {
-		if (valor.getValor() == null)
-			return new ResponseEntity<>(valor, HttpStatus.NO_CONTENT);
-		
-		NumeroPrimo retorno = new NumeroPrimo();
-		retorno.setValor(valor.getValor());
-		retorno.setPrimo(service.isPrimo(valor.getValor()));
-		return new ResponseEntity<>(retorno, HttpStatus.OK);
+	public ResponseEntity<NumeroPrimo> getCalculo() {
+		try {
+			NumeroPrimo primo = service.getPrimo();
+			return new ResponseEntity<>(primo, HttpStatus.OK);
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+			return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+		}
 	}
 
 	@PostMapping("/")
-	public ResponseEntity<NumeroPrimo> getCalculoPost(@RequestBody NumeroPrimo valor) {
-		return getCalculo(valor);
+	public ResponseEntity<NumeroPrimo> getCalculoPost() {
+		return getCalculo();
 	}
 }
